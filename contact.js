7549c86b2b8eeb6febfe901a1ea6d74fc4877739
arielgrodsky-ship-it <arielgrodsky@gmail.com>
2026-03-12 12:54:39 +0200

    Add files via upload

--- contact.js
+++ contact.js
@@ -5,25 +5,43 @@ function validateForm() {
     const phone = document.getElementById("Tel").value;
     const issue = document.getElementById("Issue").value;
 
+    const usernamePattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_]+$/;
+
     if (username.trim() === "") {
         alert("Please enter a username!");
         return; 
     }
 
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-        alert("Please enter a valid email address!");
+    if( username.length < 3 || username.length > 20) {
+        alert("Username must be between 3 and 20 characters!");
         return;
     }
 
-    if (age < 15 || age > 120) {
-        alert("Age must be between 15 and 120!");
+    if(!usernamePattern.test(username)) {
+        alert("The username must contain one Cap letter, one small letter, and one number.");
+        return;
+    }
+
+    const gmailPattern = /^(?!*\.\.)[a-zA-Z0-9][a-zA-Z0-9.][a-zA-Z0-9]@gmail\.com$/;
+    if (!gmailPattern.test(email)) {
+        alert("Invalid Gmail address! Make sure it ends with @gmail.com");
+        return;
+    }
+
+    const numberCheck = /[0-9]/; 
+    if (numberCheck.test(email)) {
+        alert("Your email cannot contain numbers! Letters only, please.");
+        return;
+    }
+
+    if (age < 15 || age >= 120) {
+        alert("Age must be at least 15 and less than 120!");
         return;
     }
 
     const phonePattern = /^[0-9]{10}$/; 
     if (!phonePattern.test(phone)) {
-        alert("Please enter a valid 10-digit phone number (e.g., 0501234567)!");
+        alert("Please enter a valid phone number!");
         return;
     }
 
